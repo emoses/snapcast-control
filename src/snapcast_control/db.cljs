3b9ca91904c7270a77284b8689e0624c1c1bf3d2
Evan Moses <evan.moses@okta.com>
2021-09-02 21:09:39 -0700

    Initial commit from tpl

---
+++ src/snapcast_control/db.cljs
@@ -0,0 +1,4 @@
+(ns snapcast-control.db)
+
+(def default-db
+  {:name "re-frame"})
---
+++ src/snapcast_control/views.cljs
@@ -0,0 +1,12 @@
+(ns snapcast-control.views
+  (:require
+   [re-frame.core :as re-frame]
+   [snapcast-control.subs :as subs]
+   ))
+
+(defn main-panel []
+  (let [name (re-frame/subscribe [::subs/name])]
+    [:div
+     [:h1
+      "Hello from " @name]
+     ]))
